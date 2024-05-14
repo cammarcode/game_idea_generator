1abed2d66c6e7f0fb9b3e63594afc838f69b19ea
cammarcode <nzcamm@gmail.com>
2024-05-14 12:05:19 +1200

    added all routes and pages + test db code

--- routes.py
+++ routes.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template
-
+import sqlite3
 
 app = Flask(__name__)
 db = "gamedb.db"
@@ -7,7 +7,26 @@ db = "gamedb.db"
 
 @app.route('/')
 def home():
-    return render_template("home.html")
+    conn = sqlite3.connect(db)
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM Genre WHERE id = ?", (1,))
+    genre = cur.fetchall()
+    return render_template("home.html", genre=genre)
+
+
+@app.route('/results/settings')
+def results():
+    return render_template("results.html")
+
+
+@app.route('/login')
+def login():
+    return render_template("login.html")
+
+
+@app.route('/signup')
+def signup():
+    return render_template("signup.html")
 
 
 if __name__ == "__main__":
