e4cf7cb350f37d176b90ce69eed177dff1dd9d07
cammarcode <nzcamm@gmail.com>
2024-06-10 13:38:35 +1200

    Sends info to results, now has errors from jinja in js

--- routes.py
+++ routes.py
@@ -5,6 +5,8 @@ import random
 app = Flask(__name__)
 db = "gamedb.db"
 
+def get_ids():
+    pass
 
 @app.route('/')
 def home(): #Homepage, no values loaded in
@@ -18,12 +20,18 @@ def home(): #Homepage, no values loaded in
 
 
 @app.route('/results/<settings>') 
-def results(settings): # Settings are a string of letters/numbers which represent: 
+def results(settings): # Settings are a string of letters/numbers which represent: genreamount, settingsamount, mechanicamount
+    total = [] # list of lists of genre, set, mec results
+    result = [] # each result goes here
+    genreamount, settingsamount, mechanicamount = list(map(int, str(settings).split("'n")))
+    print(genreamount, settingsamount, mechanicamount)
     conn = sqlite3.connect(db)
     cur = conn.cursor()
-    cur.execute("SELECT * FROM Genre WHERE id = ?", (settings,))
-    results = cur.fetchall()
-    return render_template("results.html", settings=settings, results=results)
+    for i in range(genreamount):
+        cur.execute("SELECT * FROM Genre WHERE id = ?", (i+1,))
+        result.append(cur.fetchall())
+    print(result)
+    return render_template("results.html", results=result)
 
 
 @app.route('/login')
@@ -42,23 +50,13 @@ def triangles(size, type, chara):
     return render_template("triangles.html", size=int(size), type=type, chara=chara, spacchar=spacchar)
 
 
-#@app.route("/process/<info>")
-#def process(info):
-    print('hit')
-    settings, genreinfo, settinginfo, mechanicinfo = deconstruct(info)      
-    resp = app.make_response(render_template("home.html", settings=settings, test=1, genreinfo=genreinfo, settinginfo=settinginfo, mechanicinfo=mechanicinfo))
-    return resp
 
 @app.route('/process', methods=['POST']) 
 def process(): 
     data = request.get_json() # retrieve the data sent from JavaScript 
-    # process the data using Python code 
     result = data['value'] * 2
     return jsonify(result=result) # return the result to JavaScript 
 
-#def deconstruct(info):
-#    print(str(info).split())
-#    return list(map(int,str(info).split()))
 
 
 if __name__ == "__main__":
